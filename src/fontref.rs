44ae72dac88d9aa2d6f74efe766284124035064c
Rasmus Kaj <kaj@kth.se>
2015-10-13 09:34:47 +0200

    Move fontref to separate file.

---
+++ src/fontref.rs
@@ -0,0 +1,48 @@
+use std::fmt;
+use std::sync::Arc;
+
+use ::fontmetrics::FontMetrics;
+use ::encoding::WIN_ANSI_ENCODING;
+
+/// A font ready to be used in a TextObject.
+///
+/// The way to get FontRef is to call `Canvas::get_font` with a
+/// `FontSource`.  In PDF terms, a FontSource is a font dictionary,
+/// and a FontRef is a name registered with a font source in the page
+/// resources, like /F1.
+#[derive(Debug, PartialEq, Eq, Hash, Clone)]
+pub struct FontRef {
+    n: usize,
+    metrics: Arc<FontMetrics>
+}
+
+impl FontRef {
+    pub fn new(n: usize, metrics: Arc<FontMetrics>) -> FontRef {
+        FontRef {
+            n: n,
+            metrics: metrics
+        }
+    }
+
+    /// Get the width of the given text in this font at given size.
+    pub fn get_width(&self, size: f32, text: &str) -> f32 {
+        size * self.get_width_raw(text) as f32 / 1000.0
+    }
+
+    /// Get the width of the given text in thousands of unit of text
+    /// space.  This unit is what is used in some places internally in
+    /// pdf files and in some methods on a `TextObject`.
+    pub fn get_width_raw(&self, text: &str) -> u32 {
+        let mut result = 0;
+        for char in WIN_ANSI_ENCODING.encode_string(text) {
+            result += self.metrics.get_width(char).unwrap_or(100) as u32;
+        }
+        result
+    }
+}
+
+impl fmt::Display for FontRef {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "/F{}", self.n)
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -20,11 +20,13 @@ extern crate lazy_static;
 extern crate time;
 
 use std::io::{Seek, SeekFrom, Write, self};
-use std::fmt;
 use std::collections::HashMap;
 use std::collections::BTreeMap;
 use std::fs::File;
 use std::sync::Arc;
+
+mod fontref;
+pub use ::fontref::FontRef;
 
 mod fontmetrics;
 pub use ::fontmetrics::FontMetrics;
@@ -140,41 +142,6 @@ impl FontSource {
     }
 }
 
-/// A font ready to be used in a TextObject.
-///
-/// The way to get FontRef is to call `Canvas::get_font` with a
-/// `FontSource`.  In PDF terms, a FontSource is a font dictionary,
-/// and a FontRef is a name registered with a font source in the page
-/// resources, like /F1.
-#[derive(Debug, PartialEq, Eq, Hash, Clone)]
-pub struct FontRef {
-    n: usize,
-    metrics: Arc<FontMetrics>
-}
-
-impl FontRef {
-    /// Get the width of the given text in this font at given size.
-    pub fn get_width(&self, size: f32, text: &str) -> f32 {
-        size * self.get_width_raw(text) as f32 / 1000.0
-    }
-
-    /// Get the width of the given text in thousands of unit of text
-    /// space.  This unit is what is used in some places internally in
-    /// pdf files and in some methods on a `TextObject`.
-    pub fn get_width_raw(&self, text: &str) -> u32 {
-        let mut result = 0;
-        for char in WIN_ANSI_ENCODING.encode_string(text) {
-            result += self.metrics.get_width(char).unwrap_or(100) as u32;
-        }
-        result
-    }
-}
-
-impl fmt::Display for FontRef {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "/F{}", self.n)
-    }
-}
 
 /// An visual area where content can be drawn (a page).
 ///
@@ -521,7 +488,7 @@ impl<'a> Canvas<'a> {
             return r.clone();
         }
         let n = self.fonts.len();
-        let r = FontRef { n: n, metrics: Arc::new(font.get_metrics().unwrap()) };
+        let r = FontRef::new(n, Arc::new(font.get_metrics().unwrap()));
         self.fonts.insert(font, r.clone());
         r
     }
