2706649046d54e4c02a7d684ce99518fe43f5f1f
hummingly <31522351+hummingly@users.noreply.github.com>
2018-07-13 22:39:42 +0200

    Adds ZapfDingbats encoding
    Support for the ZapfDingbats font was added. Additionally, the text example now conforms to the PDF standard.

--- src/encoding.rs
+++ src/encoding.rs
@@ -420,6 +420,229 @@ lazy_static! {
             unicode_to_code: codes
         }
     };
+
+    // https://unicode.org/Public/MAPPINGS/VENDORS/ADOBE/zdingbat.txt
+    pub static ref ZAPFDINGBATS_ENCODING: Encoding = {
+        let mut codes = BTreeMap::new();
+        let mut names = BTreeMap::new();
+        for code in 32..255 {
+            codes.insert(code as char, code);
+        }
+        {
+            let mut enc = |ch: char, name: &'static str, code: u8| {
+                codes.insert(ch, code);
+                names.insert(name, code);
+            };
+            enc(' ', "space", 0o40);
+            enc(' ', "space", 0o40);
+            enc('✁', "a1", 0o41);
+            enc('✂', "a2", 0o42);
+            enc('✃', "a202", 0o43);
+            enc('✄', "a3", 0o44);
+            enc('☎', "a4", 0o45);
+            enc('✆', "a5", 0o46);
+            enc('✇', "a119", 0o47);
+            enc('✈', "a118", 0o50);
+            enc('✉', "a117", 0o51);
+            enc('☛', "a11", 0o52);
+            enc('☞', "a12", 0o53);
+            enc('✌', "a13", 0o54);
+            enc('✍', "a14", 0o55);
+            enc('✎', "a15", 0o56);
+            enc('✏', "a16", 0o57);
+            enc('✐', "a105", 0o60);
+            enc('✑', "a17", 0o61);
+            enc('✒', "a18", 0o62);
+            enc('✓', "a19", 0o63);
+            enc('✔', "a20", 0o64);
+            enc('✕', "a21", 0o65);
+            enc('✖', "a22", 0o66);
+            enc('✗', "a23", 0o67);
+            enc('✘', "a24", 0o70);
+            enc('✙', "a25", 0o71);
+            enc('✚', "a26", 0o72);
+            enc('✛', "a27", 0o73);
+            enc('✜', "a28", 0o74);
+            enc('✝', "a6", 0o75);
+            enc('✞', "a7", 0o76);
+            enc('✟', "a8", 0o77);
+            enc('✠', "a9", 0o100);
+            enc('✡', "a10", 0o101);
+            enc('✢', "a29", 0o102);
+            enc('✣', "a30", 0o103);
+            enc('✤', "a31", 0o104);
+            enc('✥', "a32", 0o105);
+            enc('✦', "a33", 0o106);
+            enc('✧', "a34", 0o107);
+            enc('★', "a35", 0o110);
+            enc('✩', "a36", 0o111);
+            enc('✪', "a37", 0o112);
+            enc('✫', "a38", 0o113);
+            enc('✬', "a39", 0o114);
+            enc('✭', "a40", 0o115);
+            enc('✮', "a41", 0o116);
+            enc('✯', "a42", 0o117);
+            enc('✰', "a43", 0o120);
+            enc('✱', "a44", 0o121);
+            enc('✲', "a45", 0o122);
+            enc('✳', "a46", 0o123);
+            enc('✴', "a47", 0o124);
+            enc('✵', "a48", 0o125);
+            enc('✶', "a49", 0o126);
+            enc('✷', "a50", 0o127);
+            enc('✸', "a51", 0o130);
+            enc('✹', "a52", 0o131);
+            enc('✺', "a53", 0o132);
+            enc('✻', "a54", 0o133);
+            enc('✼', "a55", 0o134);
+            enc('✽', "a56", 0o135);
+            enc('✾', "a57", 0o136);
+            enc('✿', "a58", 0o137);
+            enc('❀', "a59", 0o140);
+            enc('❁', "a60", 0o141);
+            enc('❂', "a61", 0o142);
+            enc('❃', "a62", 0o143);
+            enc('❄', "a63", 0o144);
+            enc('❅', "a64", 0o145);
+            enc('❆', "a65", 0o146);
+            enc('❇', "a66", 0o147);
+            enc('❈', "a67", 0o150);
+            enc('❉', "a68", 0o151);
+            enc('❊', "a69", 0o152);
+            enc('❋', "a70", 0o153);
+            enc('●', "a71", 0o154);
+            enc('❍', "a72", 0o155);
+            enc('■', "a73", 0o156);
+            enc('❏', "a74", 0o157);
+            enc('❐', "a203", 0o160);
+            enc('❑', "a75", 0o161);
+            enc('❒', "a204", 0o162);
+            enc('▲', "a76", 0o163);
+            enc('▼', "a77", 0o164);
+            enc('◆', "a78", 0o165);
+            enc('❖', "a79", 0o166);
+            enc('◗', "a81", 0o167);
+            enc('❘', "a82", 0o170);
+            enc('❙', "a83", 0o171);
+            enc('❚', "a84", 0o172);
+            enc('❛', "a97", 0o173);
+            enc('❜', "a98", 0o174);
+            enc('❝', "a99", 0o175);
+            enc('❞', "a100", 0o176);
+            enc('❨', "a89", 0o200); // Note: (CUS) cannot be displayed
+            enc('❩', "a90", 0o201); // Note: (CUS) cannot be displayed
+            enc('❪', "a93", 0o202); // Note: (CUS) cannot be displayed
+            enc('❫', "a94", 0o203); // Note: (CUS) cannot be displayed
+            enc('❬', "a91", 0o204); // Note: (CUS) cannot be displayed
+            enc('❭', "a92", 0o205); // Note: (CUS) cannot be displayed
+            enc('❮', "a205", 0o206); // Note: (CUS) cannot be displayed
+            enc('❯', "a85", 0o207); // Note: (CUS) cannot be displayed
+            enc('❰', "a206", 0o210); // Note: (CUS) cannot be displayed
+            enc('❱', "a86", 0o211); // Note: (CUS) cannot be displayed
+            enc('❲', "a87", 0o212); // Note: (CUS) cannot be displayed
+            enc('❳', "a88", 0o213); // Note: (CUS) cannot be displayed
+            enc('❴', "a95", 0o214); // Note: (CUS) cannot be displayed
+            enc('❵', "a96", 0o215); // Note: (CUS) cannot be displayed
+            enc('❡', "a101", 0o241);
+            enc('❢', "a102", 0o242);
+            enc('❣', "a103", 0o243);
+            enc('❤', "a104", 0o244);
+            enc('❥', "a106", 0o245);
+            enc('❦', "a107", 0o246);
+            enc('❧', "a108", 0o247);
+            enc('♣', "a112", 0o250);
+            enc('♦', "a111", 0o251);
+            enc('♥', "a110", 0o252);
+            enc('♠', "a109", 0o253);
+            enc('①', "a120", 0o254);
+            enc('②', "a121", 0o255);
+            enc('③', "a122", 0o256);
+            enc('④', "a123", 0o257);
+            enc('⑤', "a124", 0o260);
+            enc('⑥', "a125", 0o261);
+            enc('⑦', "a126", 0o262);
+            enc('⑧', "a127", 0o263);
+            enc('⑨', "a128", 0o264);
+            enc('⑩', "a129", 0o265);
+            enc('❶', "a130", 0o266);
+            enc('❷', "a131", 0o267);
+            enc('❸', "a132", 0o270);
+            enc('❹', "a133", 0o271);
+            enc('❺', "a134", 0o272);
+            enc('❻', "a135", 0o273);
+            enc('❼', "a136", 0o274);
+            enc('❽', "a137", 0o275);
+            enc('❾', "a138", 0o276);
+            enc('❿', "a139", 0o277);
+            enc('➀', "a140", 0o300);
+            enc('➁', "a141", 0o301);
+            enc('➂', "a142", 0o302);
+            enc('➃', "a143", 0o303);
+            enc('➄', "a144", 0o304);
+            enc('➅', "a145", 0o305);
+            enc('➆', "a146", 0o306);
+            enc('➇', "a147", 0o307);
+            enc('➈', "a148", 0o310);
+            enc('➉', "a149", 0o311);
+            enc('➊', "a150", 0o312);
+            enc('➋', "a151", 0o313);
+            enc('➌', "a152", 0o314);
+            enc('➍', "a153", 0o315);
+            enc('➎', "a154", 0o316);
+            enc('➏', "a155", 0o317);
+            enc('➐', "a156", 0o320);
+            enc('➑', "a157", 0o321);
+            enc('➒', "a158", 0o322);
+            enc('➓', "a159", 0o323);
+            enc('➔', "a160", 0o324);
+            enc('→', "a161", 0o325);
+            enc('↔', "a163", 0o326);
+            enc('↕', "a164", 0o327);
+            enc('➘', "a196", 0o330);
+            enc('➙', "a165", 0o331);
+            enc('➚', "a192", 0o332);
+            enc('➛', "a166", 0o333);
+            enc('➜', "a167", 0o334);
+            enc('➝', "a168", 0o335);
+            enc('➞', "a169", 0o336);
+            enc('➟', "a170", 0o337);
+            enc('➠', "a171", 0o340);
+            enc('➡', "a172", 0o341);
+            enc('➢', "a173", 0o342);
+            enc('➣', "a162", 0o343);
+            enc('➤', "a174", 0o344);
+            enc('➥', "a175", 0o345);
+            enc('➦', "a176", 0o346);
+            enc('➧', "a177", 0o347);
+            enc('➨', "a178", 0o350);
+            enc('➩', "a179", 0o351);
+            enc('➪', "a193", 0o352);
+            enc('➫', "a180", 0o353);
+            enc('➬', "a199", 0o354);
+            enc('➭', "a181", 0o355);
+            enc('➮', "a200", 0o356);
+            enc('➯', "a182", 0o357);
+            enc('➱', "a201", 0o361);
+            enc('➲', "a183", 0o362);
+            enc('➳', "a184", 0o363);
+            enc('➴', "a197", 0o364);
+            enc('➵', "a185", 0o365);
+            enc('➶', "a194", 0o366);
+            enc('➷', "a198", 0o367);
+            enc('➸', "a186", 0o370);
+            enc('➹', "a195", 0o371);
+            enc('➺', "a187", 0o372);
+            enc('➻', "a188", 0o373);
+            enc('➼', "a189", 0o374);
+            enc('➽', "a190", 0o375);
+            enc('➾', "a191", 0o376);
+        }
+        Encoding {
+            name: "ZapfDingbatsEncoding".to_string(),
+            name_to_code: names,
+            unicode_to_code: codes
+        }
+    };
 }
 
 #[test]
